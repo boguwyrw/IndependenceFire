ecd67e211649d7169a7259f3772cdf5291427068
boguwyrw <b.wyrwal@wp.pl>
2024-01-29 21:45:49 +0100

    Input system has been added. Player movement functionality has been made.

--- IndependenceFire/Assets/Scripts/PlayerMovement.cs
+++ IndependenceFire/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    private void Start()
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private float movementSpeed = 4.0f;
+
+    private PlayerInput playerInput = null;
+    private Vector2 moveVector = Vector2.zero;
+
+    private void Awake()
     {
-        
+        playerInput = new PlayerInput();
     }
 
     private void Update()
     {
-        
+        Vector3 movement = new Vector3(moveVector.x, 0.0f, moveVector.y);
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        playerInput.Player.Movement.performed += OnMovementPerformed;
+        playerInput.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        playerInput.Player.Movement.performed -= OnMovementPerformed;
+        playerInput.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext context)
+    {
+        moveVector = Vector2.zero;
     }
 }
